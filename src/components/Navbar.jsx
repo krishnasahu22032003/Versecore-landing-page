5c35fe87500956fee9b3b6179610e7fa52396164
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-27 00:47:08 +0530

    Tailwind classes added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { motion } from "motion/react"
+import styles from "../../constants/style"
+import { navVariants } from "../../constants/motion"
+
+const Navbar = () => {
+  return (
+ <motion.nav variants={navVariants} >
+
+ </motion.nav>
+  )
+}
+
+export default Navbar
