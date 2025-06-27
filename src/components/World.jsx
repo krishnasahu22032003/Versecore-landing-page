a12bd62b25541a3f09995e59700f104027385596
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-27 17:16:23 +0530

    World section images added

---
+++ src/components/World.jsx
@@ -0,0 +1,15 @@
+'use client'
+import { motion } from "motion/react"
+import styles from "../../constants/style"
+import { TitleText,TypingText } from "./CustomTexts"
+import { fadeIn,staggerContainer } from "../../constants/motion"
+
+const World = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default World
