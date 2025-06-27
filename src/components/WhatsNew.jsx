f7d53d9ee73c1a4345d7cd0eaaa748267a717ef2
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-27 17:07:44 +0530

    Newfeature component created

--- src/components/WhatsNew.jsx
+++ src/components/WhatsNew.jsx
@@ -1,5 +1,7 @@
 'use client'
-
+import styles from "../../constants/style"
+import { motion } from "motion/react"
+import { newFeatures } from "../../constants"
 const WhatsNew = () => {
   return (
     <div>
