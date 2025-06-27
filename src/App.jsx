816fc81ee809d76cfe007ef28b420444461277c6
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-28 01:54:26 +0530

    App.jsx improved

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,18 @@ function App() {
         <div className='gradient-03 z-0' />
         <Explore />
       </div>
+      <div className='relative'>
       <GetStarted/>
+      <div className='gradient-04 z-0'/>
       <WhatsNew/>
+      </div>
       <World/>
+      <div className='relative ' >
       <Insights/>
+      <div className='gradient-04 z-0' />
       <Feedback/>
+
+      </div>
       <Footer/>
     </div>
 
